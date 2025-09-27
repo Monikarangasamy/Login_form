4277ce622053fc40685b43099f1d4ecf88924865
Monika <rsamy0858@gmail.com>
2025-09-27 10:14:40 +0530

    server login in express

--- dataBase.js
+++ dataBase.js
@@ -1,14 +1,29 @@
 const mongoose = require("mongoose");
 
-mongoose.set('strictQuery' ,false)
-mongoose.connect("mongodb://127.0.0.1:27017/loginApp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/loginApp");
+
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+async function findUserByUsername(username) {
+  return await User.findOne({ username });
+}
+
+async function findUserForLogin(username, password) {
+  return await User.findOne({ username, password });
+}
+
+async function createNewUser(username, password) {
+  const newUser = new User({ username, password });
+  await newUser.save();
+}
+
+module.exports = {
+  findUserByUsername,
+  findUserForLogin,
+  createNewUser,
+};
--- server.js
+++ server.js
@@ -1,67 +1,45 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const querystring = require("querystring");
-const User = require("./dataBase.js");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { findUserByUsername, findUserForLogin, createNewUser } = require('./database.js');
 
-function serveFile(res, filePath, contentType) {
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Server Error");
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content);
-    }
-  });
-}
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      serveFile(res, path.join(__dirname, "public", "signup.html"), "text/html");
-    } else if (req.url === "/index") {
-      serveFile(res, path.join(__dirname, "public", "index.html"), "text/html");
-    } else if (req.url === "/style.css") {
-      serveFile(res, path.join(__dirname, "public", "style.css"), "text/css");
-    } else {
-      res.writeHead(404);
-      res.end("Not Found");
-    }
-  }
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  if (req.method === "POST") {
-    let body = "";
-    req.on("data", chunk => {
-      body += chunk.toString();
-    });
-    req.on("end", async () => {
-      const formData = querystring.parse(body);
-      const { username, password } = formData;
-
-      if (req.url === "/signup") {
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-          res.end("Username already exists!");
-        } else {
-          const newUser = new User({ username, password });
-          await newUser.save();
-          res.end("Signup successful. You can now login.");
-        }
-      }
-
-      if (req.url === "/index") {
-        const user = await User.findOne({ username, password });
-        if (user) {
-          res.end("Login successful, Welcome " + username);
-        } else {
-          res.end("Invalid username or password!");
-        }
-      }
-    });
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  const existingUser = await findUserByUsername(username);
+
+  if (existingUser) {
+    return res.send('Username already exists!');
+  }
+
+  await createNewUser(username, password);
+  res.send('Signup successful. You can now login.');
+});
+
+app.post('/index', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await findUserForLogin(username, password);
+
+  if (user) {
+    return res.send(`Login successful, Welcome ${username}`);
+  }
+  res.send('Invalid username or password!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
